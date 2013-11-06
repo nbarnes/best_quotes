6fab80fa08eb833323fff33d7ed5f289b43c4b98
NBarnes <nbarnes@gmail.com>
2013-11-06 15:48:19 -0800

    End chapter 5

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,40 @@
 class QuotesController < Rulers::Controller
+
   def a_quote
     render :a_quote, :noun => :winking
   end
+
+  def quote_1
+    quote_1 = Rulers::Model::FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+
+  def quote_2
+    quote_2 = Rulers::Model::FileModel.find(2)
+    render :quote, :obj => quote_2
+  end
+
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth a thousand pixels",
+      "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
+
+  def update
+  end
+
+  def show
+    quote = FileModel.find(params["id"])
+    render :quote, :obj => quote
+  end
+
 end
